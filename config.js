c6c75b44e3a92a1fe64ec8f165e6ef6bdaf4eb3d
jaso0909 <162663327+jaso0909@users.noreply.github.com>
2024-03-08 10:06:41 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+global.owner = [king👑😈
+  ['+966574817645', 'king', true],
+  ['6281255369012', 'king', true], 
   [''] 
 ] //Number of owners
 
